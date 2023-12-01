12079384d5d9441d54a88637d676e103e1614240
Oscar Andres Gomez <os.gomez.lozano@gmail.com>
2023-11-30 23:22:14 -0500

    langton's ant, not finished

---
+++ langtons-ant/src/main.rs
@@ -0,0 +1,70 @@
+use macroquad::{miniquad::window::set_window_size, prelude::*};
+
+fn plot_grid(step: &f32, window_size: &f32, on_x: bool) {
+    let mut i = *step;
+    while i < *window_size {
+        if on_x {
+            draw_line(0., i, *window_size, i, 1.1, WHITE);
+        } else {
+            draw_line(i, 0., i, *window_size, 1.1, WHITE);
+        }
+        i += step;
+    }
+}
+
+#[macroquad::main("Langton's ant")]
+async fn main() {
+    set_window_size(600, 600);
+    let window_size = 600.;
+    let step = 20.;
+    let total_divisions = window_size / step;
+
+    //Moves left, up, right, down
+    let moves: Vec<(f32, f32)> = vec![(-step, 0.), (0., -step), (step, 0.), (0., step)];
+
+    let mut dir: i32 = 1;
+
+    let mut map: Vec<Vec<bool>> =
+        vec![vec![true; total_divisions as usize]; total_divisions as usize];
+
+    //Ant attributes
+    let mut pos_x = window_size / 2.;
+    let mut pos_y = window_size / 2.;
+
+    map[5][5] = false;
+
+    loop {
+        plot_grid(&step, &window_size, true);
+        plot_grid(&step, &window_size, false);
+
+        //My eyes hurts right now
+        if map[(pos_y / step) as usize][(pos_x / step) as usize] {
+            map[(pos_y / step) as usize][(pos_x / step) as usize] = false;
+
+            dir += 1;
+            dir = dir % moves.len() as i32;
+        } else {
+            map[(pos_y / step) as usize][(pos_x / step) as usize] = true;
+
+            dir -= 1;
+            dir = dir.abs() % moves.len() as i32;
+        }
+
+        pos_x += moves[dir as usize].0;
+        pos_y += moves[dir as usize].1;
+
+        for i in 0..map.len() {
+            for j in 0..map[0].len() {
+                if map[i][j] {
+                    draw_rectangle(j as f32 * step, i as f32 * step, step, step, WHITE);
+                } else {
+                    draw_rectangle(j as f32 * step, i as f32 * step, step, step, BLACK);
+                }
+            }
+        }
+
+        draw_circle(pos_x, pos_y, 20., RED);
+
+        next_frame().await;
+    }
+}

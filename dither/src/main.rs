e5a00042374c07b3a58fd714b98b9087a0417cc1
Oscar Andres Gomez <os.gomez.lozano@gmail.com>
2023-12-04 12:36:44 -0500

    Still having problems with dithering

--- dither/src/main.rs
+++ dither/src/main.rs
@@ -1,11 +1,8 @@
+use std::mem;
+
 use macroquad::prelude::*;
 
-fn binary_length(number: u64) -> usize {
-    let binary_string = format!("{:b}", number);
-    binary_string.len()
-}
-
-fn bytes_as_binary<T: std::fmt::Binary>(value: T) -> String {
+fn _bytes_as_binary<T: std::fmt::Binary>(value: T) -> String {
     let size_of_value = std::mem::size_of::<T>();
     let value_bytes: &[u8] =
         unsafe { std::slice::from_raw_parts(&value as *const T as *const u8, size_of_value) };
@@ -20,35 +17,43 @@ fn bytes_as_binary<T: std::fmt::Binary>(value: T) -> String {
 
 fn interleave_bits_64(x: u64, y: u64) -> u64 {
     let mut result = 0;
-    let mut mask = 1;
 
-    println!("x: {}", bytes_as_binary(x));
-    println!("y: {}", bytes_as_binary(y));
+    //println!("x: {}", bytes_as_binary(x));
+    //println!("y: {}", bytes_as_binary(y));
 
-    for i in 0..binary_length(x) {
-        println!("{}", bytes_as_binary(result));
-        result |= ((x & mask) << i) | ((y & mask) << (i + 1));
-        mask <<= 1;
+    for i in 0..mem::size_of_val(&x) {
+        //println!("inside: {}", bytes_as_binary(result));
+        let x_masked = x & (1 << i);
+        let y_masked = y & (1 << i);
+        result |= x_masked << i;
+        result |= y_masked << (i + 1);
     }
 
-    println!("{}", bytes_as_binary(result));
+    //println!("return: {}", bytes_as_binary(result));
 
     result
 }
 
 fn generate_treshold_matrix(m: u64) -> Vec<Vec<u64>> {
+    //I need to fix this as this is not generating properly the matrix
+    //I thinks that I can just use a single for working as a counter
+    //and then start adding
+
+    //Nope, that was'nt the problem
+
+    //wikipedia page: https://en.wikipedia.org/wiki/Ordered_dithering#cite_note-2
     let mut res: Vec<Vec<u64>> = Vec::new();
 
     for i in 0..m {
         let mut tmp: Vec<u64> = Vec::new();
         for j in 0..m {
-            let val = interleave_bits_64(i ^ j, i);
-            tmp.push((val / (m * m) as u64).reverse_bits());
+            let val = interleave_bits_64(i ^ j, j);
+            tmp.push(((val).reverse_bits()) / (m * m));
         }
         res.push(tmp);
     }
 
-    return res;
+    res
 }
 
 fn _quantizise(_pixel: Color) -> u64 {
@@ -69,9 +74,9 @@ async fn main() {
 
     request_new_screen_size(image.width() as f32, image.height() as f32);
 
-    assert_eq!(0b00001111, interleave_bits_64(0b00000010, 0b00000110));
+    //assert_eq!(0b10101010, interleave_bits_64(0b0000, 0b1111));
 
-    let m_size: u64 = 2;
+    let m_size: u64 = 8;
 
     let treshold_map = generate_treshold_matrix(m_size);
 
